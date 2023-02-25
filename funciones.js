3c55ac06d8a868483fb090fd06a33af62af7c1fa
AgustinaCastillo <126342803+AgustinaCastillo@users.noreply.github.com>
2023-02-25 13:19:34 -0300

    Proyecto final Argenitna Programa

---
+++ funciones.js
@@ -0,0 +1,25 @@
+//Cargando la API "randomuser"
+fetch("https://randomuser.me/api/")
+.then(response => response.json())
+.then(data => {
+  const user = data.results[0];
+  
+  document.getElementById("avatar").src = user.picture.large;
+  document.getElementById("name").innerHTML = user.name.first + " " + user.name.last;
+  document.getElementById("date").innerHTML = user.dob.date.slice(0, 10);
+  document.getElementById("email").innerHTML = user.email;
+  document.getElementById("phone").innerHTML = user.phone;
+  document.getElementById("edad").innerHTML= user.dob.age;
+  document.getElementById("location").innerHTML = user.location.street.name + " " + user.location.street.number + ", " + user.location.state  + ", " +  user.location.country;
+});
+
+// Animacion de iconos
+function showInfo(id) {
+  var info = document.getElementById(id);
+  info.classList.remove('hidden');
+}
+
+function hideInfo(id) {
+  var info = document.getElementById(id);
+  info.classList.add('hidden');
+}
